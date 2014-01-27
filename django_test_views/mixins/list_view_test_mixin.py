be4b7d224f6a91eb435cf89964ee83d5e43ba938
Jon Bolt <epic.bagel@gmail.com>
2014-01-27 09:21:48 +0000

    Don't reproduce data

--- django_test_views/mixins/list_view_test_mixin.py
+++ django_test_views/mixins/list_view_test_mixin.py
@@ -4,22 +4,24 @@ class ListViewTestMixin(BaseObjectMixin):
 	# Generate a list of objects from the factory and the object kwargs
 	# Test to see if a set of objects appears in a view
 	def get_factory_kwargs(self):
-		return {"size" : 10}
+		kwargs = {}#super(ListViewTestMixin, self).get_factory_kwargs()
+		kwargs.update({"size" : 10})
+		return kwargs
 	def test_view(self):
 		self.should_redirect_to_login_when_anonymous()
 		# Login and generate the data
 		self.login(self.user)
-		self.generate_data()
+		data = self.generate_data()
 		response = self.should_be_callable_when_authenticated(self.user)
 		# Check that the item appears in the list view
 		self.assertEqual(
 			# List of items on the page
 			sorted([object.pk for object in response.context['object_list']]),
 			# Expected list of items, force to a list object
-			sorted([object.pk for object in self.get_data()])
+			sorted([object.pk for object in data])
 		)
 		# Ensure that an empty list wasn't passed
 		self.assertNotEqual(len(self.get_data()), 0)
-		
+
 
 
