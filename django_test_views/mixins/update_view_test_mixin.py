b720686c6c80e4ba168fde79930f36ff8a60f5c8
Jon Bolt <epic.bagel@gmail.com>
2014-03-05 14:47:23 +0000

    Remove anon user check

--- django_test_views/mixins/update_view_test_mixin.py
+++ django_test_views/mixins/update_view_test_mixin.py
@@ -17,9 +17,9 @@ class UpdateViewTestMixin(SingleObjectTestMixin):
 		self.client.logout()
 		self.should_redirect_to_login_when_anonymous()
 		# Check we cannot access when authanticated
-		if self.create_unauthorized_user():
-			response = self.is_callable(user = self.user)
-			self.assertEqual(response.status_code, 302)
+		#if self.create_unauthorized_user():
+		#	response = self.is_callable(user = self.user)
+		#	self.assertEqual(response.status_code, 302)
 
 
 
